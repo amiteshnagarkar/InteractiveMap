cfb1718114175ca78bb1da0a831a00e0dc03b878
amiteshnagarkar <amiteshnagarkar@gmail.com>
2020-06-28 18:10:12 +0100

    callout bubble now working when location is tapped

--- InteractiveMap/MapView.swift
+++ InteractiveMap/MapView.swift
@@ -14,6 +14,10 @@ struct MapView: UIViewRepresentable {
     
      let map = MKMapView()
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         
         let mapView = MKMapView()
@@ -26,7 +30,7 @@ struct MapView: UIViewRepresentable {
         let region = MKCoordinateRegion(
              center: oahuCenter.coordinate,
              latitudinalMeters: 50000,
-             longitudinalMeters: 60000)ls
+             longitudinalMeters: 60000)
         //let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
         
         map.region = region
@@ -40,6 +44,11 @@ struct MapView: UIViewRepresentable {
         
         //return mapView
         
+       // Coordinator2. = self
+        //map.delegate = context.coordinator
+        //map.delegate = context.self
+        //map.delegate = context.coordinator
+        map.delegate = context.coordinator
         
         // Show artwork on map
         let artwork = Artwork(
@@ -58,4 +67,53 @@ struct MapView: UIViewRepresentable {
 
     func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
     }
+
+    
+    class Coordinator: NSObject, MKMapViewDelegate{
+      // 1
+        var parent: MapView
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        
+      func mapView(
+        _ mapView: MKMapView,
+        viewFor annotation: MKAnnotation
+      ) -> MKAnnotationView? {
+        // 2
+        guard let annotation = annotation as? Artwork else {
+          return nil
+        }
+        // 3
+        let identifier = "artwork"
+        var view: MKMarkerAnnotationView
+        // 4
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(
+          withIdentifier: identifier) as? MKMarkerAnnotationView {
+          dequeuedView.annotation = annotation
+          view = dequeuedView
+        } else {
+          // 5
+          view = MKMarkerAnnotationView(
+            annotation: annotation,
+            reuseIdentifier: identifier)
+          view.canShowCallout = true
+          view.calloutOffset = CGPoint(x: -5, y: 5)
+          view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+      }
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
